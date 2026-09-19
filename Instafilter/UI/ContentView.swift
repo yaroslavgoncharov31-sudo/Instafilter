27f1f92188806021371d9a8f721fd259bfbc29e5
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-19 12:13:06 +0200

    feat: add buttons to rotate image,  review request display and ShareLink to share filtered image

--- Instafilter/UI/ContentView.swift
+++ Instafilter/UI/ContentView.swift
@@ -1,17 +1,19 @@
 import PhotosUI
 import SwiftUI
+import StoreKit
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var viewModel = ContentViewModel()
     @State private var showingFilters = false
+    @Environment(\.requestReview) private var requestReview
 
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
-                PhotosPicker(selection: $viewModel.selectedItem) {
+                PhotosPicker(selection: $viewModel.selectedItem, matching: .images) {
                     if let processedImage = viewModel.processedImage {
                         processedImage
                             .resizable()
@@ -23,6 +25,21 @@ struct ContentView: View {
                 .buttonStyle(.plain)
                 Spacer()
 
+                HStack(spacing: 30) {
+                    Button {
+                        viewModel.rotateCounterClockwise()
+                    } label: {
+                        Image(systemName: "rotate.left")
+                    }
+                    Button {
+                        viewModel.rotateClockwise()
+                    } label: {
+                        Image(systemName: "rotate.right")
+                    }
+                }
+                .font(.title)
+                .padding(.bottom)
+
                 HStack {
                     Text("Intensity")
                     Slider(value: $viewModel.filterIntensity)
@@ -31,19 +48,26 @@ struct ContentView: View {
                     Button("Change filter") {
                         showingFilters = true
                     }
+                    .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                        ForEach(viewModel.availableFilters, id: \.id) { option in
+                            Button(option.name) {
+                                viewModel.selectFilter(option)
+                                if viewModel.shouldRequestReview {
+                                    requestReview()
+                                }
+                            }
+                        }
+                        Button("Cancel", role: .cancel) { }
+                    }
                     Spacer()
+                    if let processedImage = viewModel.processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
                 }
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
-            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
-                ForEach(viewModel.availableFilters, id: \.id) { option in
-                    Button(option.name) {
-                        viewModel.selectFilter(option)
-                    }
-                    Button("Cancel", role: .cancel) { }
-                }
-            }
+
         }
     }
 }
