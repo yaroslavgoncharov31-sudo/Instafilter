21cc8edcb93ac82be4bf65eea62380d5b0dabde8
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-19 12:11:33 +0200

    feat: add image rotation and review request logic

--- Instafilter/ViewModels/ContentViewModel.swift
+++ Instafilter/ViewModels/ContentViewModel.swift
@@ -9,26 +9,43 @@ class ContentViewModel {
     var filterIntensity: Double = 0.5 { didSet { reprocess() } }
     var currentFilter: CIFilter = CIFilter.sepiaTone() { didSet { reprocess() } }
     var selectedItem: PhotosPickerItem? { didSet { Task { await loadImage() } } }
-
+    private var filterUsageCount: Int {
+        get { UserDefaults.standard.integer(forKey: "filterUsageCount") }
+        set { UserDefaults.standard.set(newValue, forKey: "filterUsageCount")}
+    }
+    var shouldRequestReview: Bool {
+        filterUsageCount > 0 && filterUsageCount % 20 == 0
+    }
     private var beginImage: CIImage?
     private let imageProcessor = ImageProcessor()
     private let context = CIContext()
-
     let availableFilters = FilterOption.allCases
+
+    var rotationQuarterTurns: Int = 0 { didSet { reprocess() } }
+
+    func rotateClockwise() {
+        rotationQuarterTurns -= 1
+    }
+    func rotateCounterClockwise() {
+        rotationQuarterTurns += 1
+    }
 
     func selectFilter(_ option: FilterOption) {
         currentFilter = option.filter
+        filterUsageCount += 1
     }
 
     private func loadImage() async {
         beginImage = try? await imageProcessor.loadImage(item: selectedItem)
+        rotationQuarterTurns = 0
         reprocess()
     }
 
     private func reprocess() {
         guard let beginImage else { return }
+        let rotatedImage = imageProcessor.rotated(beginImage, by: rotationQuarterTurns)
         processedImage = imageProcessor.applyProcessing(
-            to: beginImage,
+            to: rotatedImage,
             currentFilter: currentFilter,
             filterIntensity: filterIntensity,
             context: context
