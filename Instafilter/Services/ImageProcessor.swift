1b7d523788c202a686b3a6745a50c0e1db75e6e7
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-19 10:06:45 +0200

    feat:  add compatibility with other filters via kCI keys and calculating the center of the image and the strenght of the applied filters

--- Instafilter/Services/ImageProcessor.swift
+++ Instafilter/Services/ImageProcessor.swift
@@ -8,12 +8,28 @@ struct ImageProcessor {
             guard let imageData = try await item?.loadTransferable(type: Data.self) else { return nil }
             guard let inputImage = UIImage(data: imageData) else  { return nil }
 
-            let beginImage = CIImage(image: inputImage)
             return CIImage(image: inputImage)
     }
     func applyProcessing(to beginImage: CIImage, currentFilter: CIFilter, filterIntensity: Double, context: CIContext) -> Image? {
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.setValue(Float(filterIntensity), forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+        let extent = beginImage.extent
+        let imageCenter = CGPoint(x: extent.midX, y: extent.midY)
+        let minSide = min(extent.width, extent.height)
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity * minSide * 0.5, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(1 + filterIntensity * (minSide * 0.05), forKey: kCIInputScaleKey)
+        }
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(cgPoint: imageCenter), forKey: kCIInputCenterKey)
+        }
+
         guard let outputImage = currentFilter.outputImage else { return nil }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
         return Image(uiImage: UIImage(cgImage: cgImage))
